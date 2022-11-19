94c155d95fc1f0a1ac92cc9b997c846c2700a462
JoniArriazu <joni.arriazu2@gmail.com>
2022-11-18 23:01:09 -0300

    heroe page by id was added with all of its functionalities

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,15 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Heroe } from '../pages/heroe/interfaces/heroes.interface';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
 
+  private baseUrl: string = environment.baseUrl; //Prestar atencion de que importe el environment normal, no el de prod.
+
   constructor( private http : HttpClient ) { }
 
-  getHeroes() {
-    return this.http.get<Heroe[]>('http://localhost:3000/heroes')
+  getHeroes() : Observable<Heroe[]>{
+    return this.http.get<Heroe[]>(`${ this.baseUrl }/heroes`)
+  }
+
+  getHeroePorId( id: string ): Observable<Heroe>{
+    return this.http.get<Heroe>(`${ this.baseUrl }/heroes/${ id }`)
   }
 }
